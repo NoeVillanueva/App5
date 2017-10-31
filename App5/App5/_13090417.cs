7b231454549bb6141131629a1497a3eb620c9456
Villanueva.!! <Villanueva.!!>
2017-10-30 23:54:10 -0600

    Add project files.

---
+++ App5/App5/_13090417.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
+using Newtonsoft.Json;
+using SQLite;
+
+namespace App5
+{
+    [Table("_13090417")]
+    public class _13090417
+    {
+        string matricula;
+        string nombre;
+        string apellidos;
+        string direccion;
+        string telefono;
+        string carrera;
+        string semestre;
+        string correo;
+        string github;
+        bool deleted;
+
+        [JsonProperty(PropertyName = "id")]
+        public string Matricula
+        {
+            get { return matricula; }
+            set { matricula = value; }
+        }
+
+        [JsonProperty(PropertyName = "Nombre")]
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        [JsonProperty(PropertyName = "Apellidos")]
+        public string Apellidos
+        {
+            get { return apellidos; }
+            set { apellidos = value; }
+        }
+
+        [JsonProperty(PropertyName = "Direccion")]
+        public string Direccion
+        {
+            get { return direccion; }
+            set { direccion = value; }
+        }
+
+        [JsonProperty(PropertyName = "Telefono")]
+        public string Telefono
+        {
+            get { return telefono; }
+            set { telefono = value; }
+        }
+
+        [JsonProperty(PropertyName = "Carrera")]
+        public string Carrera
+        {
+            get { return carrera; }
+            set { carrera = value; }
+        }
+
+        [JsonProperty(PropertyName = "Semestre")]
+        public string Semestre
+        {
+            get { return semestre; }
+            set { semestre = value; }
+        }
+
+        [JsonProperty(PropertyName = "Correo")]
+        public string Correo
+        {
+            get { return correo; }
+            set { correo = value; }
+        }
+
+        [JsonProperty(PropertyName = "Github")]
+        public string Github
+        {
+            get { return github; }
+            set { github = value; }
+        }
+
+        [JsonProperty(PropertyName = "version")]
+        public string Version { get; set; }
+
+        [JsonProperty(PropertyName = "deleted")]
+        public bool Deleted
+        {
+            get { return deleted; }
+            set { deleted = value; }
+        }
+
+    }
+}
---
+++ App5/App5/insertar.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App5
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class insertar : ContentPage
+    {
+
+        public ObservableCollection<_13090417> Items { get; set; }
+        public static MobileServiceClient Cliente;
+        public static IMobileServiceTable<_13090417> Tabla;
+//        SQLiteConnection database;
+        string picker_carreras;
+
+        public insertar()
+        {
+            InitializeComponent();
+            Cliente = new MobileServiceClient(AzureConnection.AzureURL);
+            Tabla = Cliente.GetTable<_13090417>();
+
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var datos = new _13090417
+            {
+                //Matricula = Convert.ToInt32(Entry_ID.Text),
+                Nombre = Entry_Name.Text,
+                Apellidos = Entry_Ap.Text,
+                Direccion = Entry_Dir.Text,
+                Telefono = Entry_Tel.Text,
+                Carrera = picker_carreras,
+                Semestre = Entry_Sem.Text,
+                Correo = Entry_Cor.Text,
+                Github = Entry_Git.Text
+
+
+
+            };
+            await insertar.Tabla.InsertAsync(datos);
+            
+            //await Navigation.PushAsync(new BasedeDatos());
+        }
+
+        private void carreras_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var carreras = (Picker)sender;
+            int selectedIndex = carreras.SelectedIndex;
+
+            if (selectedIndex != -1)
+            {
+                picker_carreras = (string)carreras.ItemsSource[selectedIndex];
+
+            }
+        }
+    }
+}
